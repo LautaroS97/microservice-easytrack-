d6094e863770ecb05bcb72316766a1f8a4f3a80d
Lautaro Settembrini <lautarosettembrini@hotmail.com>
2024-09-12 16:10:44 -0300

    Buses x3 1.02

--- app.js
+++ app.js
@@ -8,24 +8,50 @@ const xmlbuilder = require('xmlbuilder');
 const app = express();
 app.use(express.json()); // Para manejar el cuerpo de solicitudes POST
 
-// Variable para almacenar el XML generado para cada bus
 let latestXml = {
     bus_1: null,
     bus_2: null,
     bus_3: null
 };
 
-// Matrículas de los buses
 const buses = {
     bus_1: 'GQP413',
     bus_2: 'DPH418',
     bus_3: 'FMD808'
 };
 
-// Función para extraer datos y generar el XML para un bus en específico
-async function extractDataForBus(busKey, busMatricula, page) {
+async function extractDataForBus(busKey, busMatricula) {
+    let browser = await puppeteer.launch({ 
+        headless: true,
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+    });
+    let page = await browser.newPage();
+
     try {
         console.log(`Iniciando búsqueda para el bus ${busKey} (${busMatricula})...`);
+        await page.goto('https://avl.easytrack.com.ar/login', { waitUntil: 'domcontentloaded' });
+
+        console.log('Esperando que el formulario de login esté disponible...');
+        await page.waitForTimeout(3000); // Espera 3 segundos antes de intentar encontrar el selector
+
+        await page.waitForSelector('app-root app-login.ng-star-inserted', { timeout: 10000 });
+        await page.waitForSelector('#mat-input-0', { timeout: 5000 }); // Espera hasta 5 segundos para que aparezca el campo
+
+        console.log('Ingresando credenciales...');
+        await page.type('#mat-input-0', 'naranja2024@transportesversari');
+        await page.type('#mat-input-1', 'naranja');
+
+        console.log('Presionando Enter...');
+        await page.keyboard.press('Enter');
+
+        try {
+            console.log('Esperando la navegación después de presionar Enter...');
+            await page.waitForNavigation({ waitUntil: 'domcontentloaded', timeout: 5000 });
+        } catch (error) {
+            console.error(`Fallo al intentar iniciar sesión para ${busKey}. Intentando con el botón de inicio de sesión...`);
+            await page.click('app-root app-login.ng-star-inserted #btn-login');
+            await page.waitForNavigation({ waitUntil: 'domcontentloaded' });
+        }
 
         async function getDataFromDashboard(url) {
             console.log(`Navegando a la URL: ${url} para el bus ${busKey}`);
@@ -48,7 +74,6 @@ async function extractDataForBus(busKey, busMatricula, page) {
             }
         }
 
-        // Intentar buscar la dirección en varias URL si es necesario
         let result = await getDataFromDashboard('https://avl.easytrack.com.ar/dashboard/1000');
         if (!result.success) {
             result = await getDataFromDashboard('https://avl.easytrack.com.ar/dashboard/1007');
@@ -58,7 +83,6 @@ async function extractDataForBus(busKey, busMatricula, page) {
             }
         }
 
-        // Si se encuentra la dirección, generar el XML correspondiente
         if (result.success) {
             console.log(`Generando XML para el bus ${busKey}...`);
             const xml = xmlbuilder.create('Response')
@@ -67,64 +91,33 @@ async function extractDataForBus(busKey, busMatricula, page) {
 
             console.log(`XML generado para ${busKey}:\n${xml}`);
 
-            // Guardar el XML en la variable global
             latestXml[busKey] = xml;
         }
     } catch (error) {
         console.error(`Error al extraer la ubicación para ${busKey}:`, error);
+    } finally {
+        console.log(`Cerrando el navegador para ${busKey}...`);
+        await browser.close();
     }
 }
 
-// Manejo de la solicitud POST para actualizar el XML de todos los buses
 app.post('/update', async (req, res) => {
     console.log('Solicitud POST entrante para actualizar los XML de todos los buses');
 
-    let browser = null;
     try {
-        browser = await puppeteer.launch({ 
-            headless: true,
-            args: ['--no-sandbox', '--disable-setuid-sandbox']
-        });
-
-        const page = await browser.newPage();
-        console.log('Navegando a la URL de inicio de sesión...');
-        await page.goto('https://avl.easytrack.com.ar/login', { waitUntil: 'domcontentloaded' });
-
-        console.log('Ingresando credenciales...');
-        await page.type('#mat-input-0', 'usuarioexterno@transportesversari');
-        await page.type('#mat-input-1', 'usu4rio3xt3rn0');
-        await page.keyboard.press('Enter');
-        await page.waitForNavigation({ waitUntil: 'domcontentloaded', timeout: 5000 });
-
-        // Si hay un problema con Enter, intentar con el botón de login
-        try {
-            await page.waitForSelector('app-root app-dashboard');
-        } catch (error) {
-            console.log('Intentando hacer clic en el botón de inicio de sesión...');
-            await page.click('#btn-login');
-            await page.waitForNavigation({ waitUntil: 'domcontentloaded' });
-        }
-
-        // Ejecutar las extracciones en paralelo
         await Promise.all([
-            extractDataForBus('bus_1', buses.bus_1, page),
-            extractDataForBus('bus_2', buses.bus_2, page),
-            extractDataForBus('bus_3', buses.bus_3, page)
+            extractDataForBus('bus_1', buses.bus_1),
+            extractDataForBus('bus_2', buses.bus_2),
+            extractDataForBus('bus_3', buses.bus_3)
         ]);
 
         res.status(200).send({ message: 'XML de los buses se están actualizando.' });
     } catch (error) {
         console.error('Error al actualizar los XML de los buses:', error);
         res.status(500).send({ message: 'Error al actualizar los XML.' });
-    } finally {
-        if (browser) {
-            console.log('Cerrando el navegador...');
-            await browser.close();
-        }
     }
 });
 
-// Manejo de las solicitudes GET para cada bus
 app.get('/voice/:busKey', (req, res) => {
     const busKey = req.params.busKey;
     console.log(`Solicitud entrante a /voice/${busKey}`);
@@ -133,7 +126,6 @@ app.get('/voice/:busKey', (req, res) => {
         res.type('application/xml');
         res.send(latestXml[busKey]);
     } else {
-        // Generar un XML de error en caso de no tener datos recientes
         const xml = xmlbuilder.create('Response')
             .ele('Say', { voice: 'Polly.Andres-Neural', language: "es-MX" }, 'Lo sentimos, no se pudo obtener la información en este momento. Por favor, intente nuevamente más tarde.')
             .end({ pretty: true });
